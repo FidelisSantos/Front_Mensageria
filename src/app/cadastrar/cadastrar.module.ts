7e8363a0096b731c09371e03b3959d174bb1a032
FidelisSantos <vitorfidelissantos@gmail.com>
2022-11-27 19:38:55 -0300

    Criação do projeto

---
+++ src/app/cadastrar/cadastrar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+
+
+@NgModule({
+  declarations: [
+    CadastrarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastrarModule { }
---
+++ src/app/mensagens/mensagens.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MensagensComponent } from './mensagens/mensagens.component';
+
+
+
+@NgModule({
+  declarations: [
+    MensagensComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MensagensModule { }
