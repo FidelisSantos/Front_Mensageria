55570b68ca0f810f8f8a5af5da61cab2956acc73
arthur1786 <arthurhp2003@gmail.com>
2022-11-30 23:45:46 -0300

    CommitArthurSubindoFinal

--- src/app/cadastrar/cadastrar/cadastrar.module.ts
+++ src/app/cadastrar/cadastrar/cadastrar.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from '../app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -16,9 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     CommonModule,
     FormsModule,
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FormsModule,
   ]
 })
 export class CadastrarModule { }
